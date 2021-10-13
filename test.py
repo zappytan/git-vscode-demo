6db42d727d4b51c7be10039be307f21f7076a43c
zappytan <shi_zhonghua@hotmail.com>
2021-10-13 18:10:06 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,18 @@
+print('hello world')
+
+
+def test():
+    print('testing')
+
+
+def test2():
+    print('test2')
+
+
+for i in range(10):
+    if i < 5:
+        test()
+    else:
+        print(i*i)
+
+print('adding more lines')
